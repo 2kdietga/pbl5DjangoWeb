6af22b4350ee5c7a2ffe1456cffc80506424311f
wind <wind@gmail.com>
2026-01-16 13:35:29 +0700

    thêm các trang html tĩnh

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', views.test_view),
+    path('', views.test_view),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def test_view(request):
-    return render(request, "auth/login.html")
+    return render(request, "violations_detail.html")
