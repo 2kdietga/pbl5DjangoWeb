8aa3fa6024742047d650929578a8a9b932cf298e
wind <wind@gmail.com>
2026-01-16 13:59:11 +0700

    tách base.html ra

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def test_view(request):
-    return render(request, "violations_detail.html")
+    return render(request, "profile.html")
